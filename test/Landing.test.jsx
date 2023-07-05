8df99a1529b0eea6ab46a0fce69cbf1f0fc2e80c
CriSFV <cristina.f.valverde@gmail.com>
2023-07-05 12:20:23 +0200

    test: add podcast list and episode list test

--- test/Landing.test.jsx
+++ test/Landing.test.jsx
@@ -1,8 +1,4 @@
-import {
-  render,
-  screen,
-  fireEvent,
-} from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Landing from "../components/Landing";
 import "@testing-library/jest-dom";
 import { LoaderProvider } from "../contexts/LoadingContext";
@@ -10,15 +6,32 @@ import getPodcasts from "../api/getPodcasts";
 import {
   podcastListMock,
   podcastListMockAfterFormated,
+  podcastSelectedMock,
+  podcastDetailMock,
 } from "./mocks/podcastsMock";
 import useLocalStorage from "../hooks/useLocalStorage";
-
+import getPodcastInfo from "../api/getPodcastDetail";
+import PodcastDetail from "../components/PodcastDetail";
+import PodcastCard from "../components/PodcastCard";
+import EpisodeDetail from "../components/EpisodeDetail";
 
 jest.mock("../hooks/useLocalStorage", () => ({
   __esModule: true,
   default: jest.fn(),
 }));
+
 jest.mock("../api/getPodcasts", () => jest.fn());
+jest.mock("../api/getPodcastDetail", () => jest.fn());
+
+jest.mock("next/router", () => ({
+  ...jest.requireActual("next/router"),
+  useRouter: () => ({
+    query: {
+      trackId: "1000604730376",
+      id: "1535809342",
+    },
+  }),
+}));
 
 const LandingWithProvider = (props) => (
   <LoaderProvider>
@@ -27,6 +40,10 @@ const LandingWithProvider = (props) => (
 );
 
 describe("Landing", () => {
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+
   it("should match with snapshot", () => {
     useLocalStorage.mockImplementation(() => [Date.now() - 100]);
     useLocalStorage.mockImplementation(() => [podcastListMockAfterFormated]);
@@ -67,5 +84,57 @@ describe("Landing", () => {
     expect(input.value).toBe("doe");
     expect(screen.getByText("Podcast Jhon Doe")).toBeInTheDocument();
     expect(listCounter).toHaveTextContent(1);
+  });
+});
+
+describe("PodcastDetail", () => {
+  it("should render a podcast episodes list", () => {
+    getPodcastInfo.mockImplementation(() => {
+      podcastDetailMock;
+    });
+    getPodcasts.mockImplementation(() => {
+      podcastListMock;
+    });
+    useLocalStorage.mockImplementation(() => [podcastDetailMock]);
+    // useLocalStorage.mockImplementation(() => [podcastSelectedMock]);
+
+    render(
+      <LoaderProvider>
+        <PodcastDetail />
+      </LoaderProvider>
+    );
+    const podcastEpisodeList = screen.getByRole("table");
+    expect(podcastEpisodeList.children).toHaveLength(2);
+  });
+});
+describe("EpisodeDetail", () => {
+  it("should render an episode", () => {
+    getPodcastInfo.mockImplementation(() => {
+      podcastDetailMock;
+    });
+    getPodcasts.mockImplementation(() => {
+      podcastListMock;
+    });
+    useLocalStorage.mockImplementation(() => [podcastDetailMock]);
+
+    render(
+      <LoaderProvider>
+        <EpisodeDetail />
+      </LoaderProvider>
+    );
+    const podcastEpisodeList = screen.getByText(podcastDetailMock[1].trackName);
+    expect(podcastEpisodeList).toBeInTheDocument();
+  });
+});
+
+describe("PodcastCard", () => {
+  it("should render a podcast card", () => {
+    render(
+      <LoaderProvider>
+        <PodcastCard podcast={podcastSelectedMock} />
+      </LoaderProvider>
+    );
+    const podcastCard = screen.getByText(podcastSelectedMock.title);
+    expect(podcastCard).toBeInTheDocument();
   });
 });
--- test/mocks/podcastsMock.js
+++ test/mocks/podcastsMock.js
@@ -1,17 +1,162 @@
+export const podcastListMock = {
+  feed: {
+    author: {
+      name: { label: "iTunes Store" },
+      uri: { label: "http://www.apple.com/itunes/" },
+    },
+    entry: [
+      {
+        "im:name": { label: "The Joe Budden Podcast" },
+        "im:image": [
+          {
+            label:
+              "https://is3-ssl.mzstatic.com/image/thumb/Podcasts113/v4/f2/21/fa/f221fabd-017f-5125-633b-f1fe4f39802a/mza_182995249085044287.jpg/55x55bb.png",
+            attributes: { height: "55" },
+          },
+          {
+            label:
+              "https://is5-ssl.mzstatic.com/image/thumb/Podcasts113/v4/f2/21/fa/f221fabd-017f-5125-633b-f1fe4f39802a/mza_182995249085044287.jpg/60x60bb.png",
+            attributes: { height: "60" },
+          },
+          {
+            label:
+              "https://is1-ssl.mzstatic.com/image/thumb/Podcasts113/v4/f2/21/fa/f221fabd-017f-5125-633b-f1fe4f39802a/mza_182995249085044287.jpg/170x170bb.png",
+            attributes: { height: "170" },
+          },
+        ],
+        summary: {
+          label:
+            "Tune into Joe Budden and his friends. Follow along the crazy adventures of these very random friends.",
+        },
+        "im:price": {
+          label: "Get",
+          attributes: { amount: "0", currency: "USD" },
+        },
+        "im:contentType": { attributes: { term: "Podcast", label: "Podcast" } },
+        rights: { label: "© All rights reserved" },
+        title: { label: "The Joe Budden Podcast - The Joe Budden Network" },
+        link: {
+          attributes: {
+            rel: "alternate",
+            type: "text/html",
+            href: "https://podcasts.apple.com/us/podcast/the-joe-budden-podcast/id1535809341?uo=2",
+          },
+        },
+        id: {
+          label:
+            "https://podcasts.apple.com/us/podcast/the-joe-budden-podcast/id1535809341?uo=2",
+          attributes: { "im:id": "1535809341" },
+        },
+        "im:artist": {
+          label: "The Joe Budden Network",
+          attributes: {
+            href: "https://podcasts.apple.com/us/artist/the-joe-budden-network/1535844019?uo=2",
+          },
+        },
+        category: {
+          attributes: {
+            "im:id": "1310",
+            term: "Music",
+            scheme:
+              "https://podcasts.apple.com/us/genre/podcasts-music/id1310?uo=2",
+            label: "Music",
+          },
+        },
+        "im:releaseDate": {
+          label: "2023-07-01T00:00:00-07:00",
+          attributes: { label: "July 1, 2023" },
+        },
+      },
+      {
+        "im:name": { label: "Drink Champs" },
+        "im:image": [
+          {
+            label:
+              "https://is1-ssl.mzstatic.com/image/thumb/Podcasts126/v4/1f/ad/c7/1fadc7d4-1e22-beaa-720a-2e2988dc1521/mza_16091018887573148747.jpg/55x55bb.png",
+            attributes: { height: "55" },
+          },
+          {
+            label:
+              "https://is3-ssl.mzstatic.com/image/thumb/Podcasts126/v4/1f/ad/c7/1fadc7d4-1e22-beaa-720a-2e2988dc1521/mza_16091018887573148747.jpg/60x60bb.png",
+            attributes: { height: "60" },
+          },
+          {
+            label:
+              "https://is2-ssl.mzstatic.com/image/thumb/Podcasts126/v4/1f/ad/c7/1fadc7d4-1e22-beaa-720a-2e2988dc1521/mza_16091018887573148747.jpg/170x170bb.png",
+            attributes: { height: "170" },
+          },
+        ],
+        summary: {
+          label:
+            "Legendary Queens rapper-turned show host N.O.R.E. teams up with Miami hip-hop pioneer DJ EFN for a night of boozy conversation and boisterous storytelling. The hosts and guests engage together in fun, light-hearted conversation - looking back at their paths to success, highlighting their lives, friendships, and iconic moments within their career. In this unfiltered series, drinks are flowing and jokes are rolling - take a seat at the table with N.O.R.E & EFN. You Gotta Relax!",
+        },
+        "im:price": {
+          label: "Get",
+          attributes: { amount: "0", currency: "USD" },
+        },
+        "im:contentType": { attributes: { term: "Podcast", label: "Podcast" } },
+        rights: { label: "© 2023 Interval Presents" },
+        title: { label: "Drink Champs - Interval Presents" },
+        link: {
+          attributes: {
+            rel: "alternate",
+            type: "text/html",
+            href: "https://podcasts.apple.com/us/podcast/drink-champs/id1096830182?uo=2",
+          },
+        },
+        id: {
+          label:
+            "https://podcasts.apple.com/us/podcast/drink-champs/id1096830182?uo=2",
+          attributes: { "im:id": "1096830182" },
+        },
+        "im:artist": {
+          label: "Interval Presents",
+          attributes: {
+            href: "https://podcasts.apple.com/us/artist/iheartradio/284341002?uo=2",
+          },
+        },
+        category: {
+          attributes: {
+            "im:id": "1310",
+            term: "Music",
+            scheme:
+              "https://podcasts.apple.com/us/genre/podcasts-music/id1310?uo=2",
+            label: "Music",
+          },
+        },
+        "im:releaseDate": {
+          label: "2023-06-30T00:01:00-07:00",
+          attributes: { label: "June 30, 2023" },
+        },
+      },
+    ],
+    updated: { label: "2023-07-04T04:43:12-07:00" },
+    rights: { label: "Copyright 2008 Apple Inc." },
+    title: { label: "iTunes Store: Top Podcasts in Music" },
+    icon: { label: "http://itunes.apple.com/favicon.ico" },
+    link: [
+      {
+        attributes: {
+          rel: "alternate",
+          type: "text/html",
+          href: "https://podcasts.apple.com/WebObjects/MZStore.woa/wa/viewTop?cc=us&id=179537&popId=3",
+        },
+      },
+      {
+        attributes: {
+          rel: "self",
+          href: "https://mzstoreservices-int.itunes.apple.com/us/rss/toppodcasts/limit=2/genre=1310/json",
+        },
+      },
+    ],
+    id: {
+      label:
+        "https://mzstoreservices-int.itunes.apple.com/us/rss/toppodcasts/limit=2/genre=1310/json",
+    },
+  },
+};
 
-export const podcastListMock = {"feed":{"author":{"name":{"label":"iTunes Store"}, "uri":{"label":"http://www.apple.com/itunes/"}}, "entry":[
-  {"im:name":{"label":"The Joe Budden Podcast"}, "im:image":[
-  {"label":"https://is3-ssl.mzstatic.com/image/thumb/Podcasts113/v4/f2/21/fa/f221fabd-017f-5125-633b-f1fe4f39802a/mza_182995249085044287.jpg/55x55bb.png", "attributes":{"height":"55"}}, 
-  {"label":"https://is5-ssl.mzstatic.com/image/thumb/Podcasts113/v4/f2/21/fa/f221fabd-017f-5125-633b-f1fe4f39802a/mza_182995249085044287.jpg/60x60bb.png", "attributes":{"height":"60"}}, 
-  {"label":"https://is1-ssl.mzstatic.com/image/thumb/Podcasts113/v4/f2/21/fa/f221fabd-017f-5125-633b-f1fe4f39802a/mza_182995249085044287.jpg/170x170bb.png", "attributes":{"height":"170"}}], "summary":{"label":"Tune into Joe Budden and his friends. Follow along the crazy adventures of these very random friends."}, "im:price":{"label":"Get", "attributes":{"amount":"0", "currency":"USD"}}, "im:contentType":{"attributes":{"term":"Podcast", "label":"Podcast"}}, "rights":{"label":"© All rights reserved"}, "title":{"label":"The Joe Budden Podcast - The Joe Budden Network"}, "link":{"attributes":{"rel":"alternate", "type":"text/html", "href":"https://podcasts.apple.com/us/podcast/the-joe-budden-podcast/id1535809341?uo=2"}}, "id":{"label":"https://podcasts.apple.com/us/podcast/the-joe-budden-podcast/id1535809341?uo=2", "attributes":{"im:id":"1535809341"}}, "im:artist":{"label":"The Joe Budden Network", "attributes":{"href":"https://podcasts.apple.com/us/artist/the-joe-budden-network/1535844019?uo=2"}}, "category":{"attributes":{"im:id":"1310", "term":"Music", "scheme":"https://podcasts.apple.com/us/genre/podcasts-music/id1310?uo=2", "label":"Music"}}, "im:releaseDate":{"label":"2023-07-01T00:00:00-07:00", "attributes":{"label":"July 1, 2023"}}}, 
-  {"im:name":{"label":"Drink Champs"}, "im:image":[
-  {"label":"https://is1-ssl.mzstatic.com/image/thumb/Podcasts126/v4/1f/ad/c7/1fadc7d4-1e22-beaa-720a-2e2988dc1521/mza_16091018887573148747.jpg/55x55bb.png", "attributes":{"height":"55"}}, 
-  {"label":"https://is3-ssl.mzstatic.com/image/thumb/Podcasts126/v4/1f/ad/c7/1fadc7d4-1e22-beaa-720a-2e2988dc1521/mza_16091018887573148747.jpg/60x60bb.png", "attributes":{"height":"60"}}, 
-  {"label":"https://is2-ssl.mzstatic.com/image/thumb/Podcasts126/v4/1f/ad/c7/1fadc7d4-1e22-beaa-720a-2e2988dc1521/mza_16091018887573148747.jpg/170x170bb.png", "attributes":{"height":"170"}}], "summary":{"label":"Legendary Queens rapper-turned show host N.O.R.E. teams up with Miami hip-hop pioneer DJ EFN for a night of boozy conversation and boisterous storytelling. The hosts and guests engage together in fun, light-hearted conversation - looking back at their paths to success, highlighting their lives, friendships, and iconic moments within their career. In this unfiltered series, drinks are flowing and jokes are rolling - take a seat at the table with N.O.R.E & EFN. You Gotta Relax!"}, "im:price":{"label":"Get", "attributes":{"amount":"0", "currency":"USD"}}, "im:contentType":{"attributes":{"term":"Podcast", "label":"Podcast"}}, "rights":{"label":"© 2023 Interval Presents"}, "title":{"label":"Drink Champs - Interval Presents"}, "link":{"attributes":{"rel":"alternate", "type":"text/html", "href":"https://podcasts.apple.com/us/podcast/drink-champs/id1096830182?uo=2"}}, "id":{"label":"https://podcasts.apple.com/us/podcast/drink-champs/id1096830182?uo=2", "attributes":{"im:id":"1096830182"}}, "im:artist":{"label":"Interval Presents", "attributes":{"href":"https://podcasts.apple.com/us/artist/iheartradio/284341002?uo=2"}}, "category":{"attributes":{"im:id":"1310", "term":"Music", "scheme":"https://podcasts.apple.com/us/genre/podcasts-music/id1310?uo=2", "label":"Music"}}, "im:releaseDate":{"label":"2023-06-30T00:01:00-07:00", "attributes":{"label":"June 30, 2023"}}}], "updated":{"label":"2023-07-04T04:43:12-07:00"}, "rights":{"label":"Copyright 2008 Apple Inc."}, "title":{"label":"iTunes Store: Top Podcasts in Music"}, "icon":{"label":"http://itunes.apple.com/favicon.ico"}, "link":[
-  {"attributes":{"rel":"alternate", "type":"text/html", "href":"https://podcasts.apple.com/WebObjects/MZStore.woa/wa/viewTop?cc=us&id=179537&popId=3"}}, 
-  {"attributes":{"rel":"self", "href":"https://mzstoreservices-int.itunes.apple.com/us/rss/toppodcasts/limit=2/genre=1310/json"}}], "id":{"label":"https://mzstoreservices-int.itunes.apple.com/us/rss/toppodcasts/limit=2/genre=1310/json"}}}
-  
-  export const podcastListMockAfterFormated =[
+export const podcastListMockAfterFormated = [
   {
     id: "1535809341",
     title: "The Mary Sister Podcast",
@@ -28,5 +173,53 @@ export const podcastListMock = {"feed":{"author":{"name":{"label":"iTunes Store"
     summary:
       "Tune into John Doe and his friends. Follow along the crazy adventures of these very random friends.",
   },
-]
-export default { podcastListMock, podcastListMockAfterFormated };
+];
+export const podcastSelectedMock = {
+  id: "1535809342",
+  title: "Podcast Jhon Doe",
+  author: "Jhon Doe",
+  img: "https://is1-ssl.mzstatic.com/image/thumb/Podcasts113/v4/f2/21/fa/f221fabd-017f-5125-633b-f1fe4f39802a/mza_182995249085044287.jpg/170x170bb.png",
+  summary:
+    "Tune into John Doe and his friends. Follow along the crazy adventures of these very random friends.",
+};
+export const podcastDetailMock = [
+  {
+    id_author: 1535809342,
+    releaseDate: "2023-07-02T23:30:00Z",
+    trackId: 1535809342,
+    trackName: "Million Dollaz Worth Of Game",
+    trackTimeMillis: 5229,
+    wrapperType: "track",
+  },
+  {
+    description:
+      "On this unique episode of MDWOG we sit down with the visionary creators of the world-renowned Rolling Loud music festival, Matt and Tariq! Rolling Loud is the number one spot to be for up-and-coming hip hop artists as they consistently put on the next big thing, and all this started from two guys who liked putting on parties. As we're preparing for our up-coming festival, Gillie Fest, these two were the perfect guests to give us game about making a successful event on a massive scale, the challenges, the successes, and everything in between.\n\nYou can find every episode of this show on Apple Podcasts, Spotify or YouTube. Prime Members can listen ad-free on Amazon Music. For more, visit barstool.link/mworthofgame",
+    episodeUrl:
+      "https://dts.podtrac.com/redirect.mp3/landmark-dynamic.barstoolsports.com/stream/uOhzKomcmeZbXGUP36LDcWfl/audio.mp3?v=eyJzcmMiOiJodHRwczovL2JhcnN0b29sLXBvZGNhc3RzLnMzLmFtYXpvbmF3cy5jb20vYmFyc3Rvb2wtc3BvcnRzL21pbGxpb24tZG9sbGF6LXdvcnRoLW9mLWdhbWUvdU9oektvbWNtZVpiWEdVUDM2TERjV2ZsL0VQLTIyNy1GVC1Sb2xsaW5nLUxvdWRfYXVkaW8uNjk0NjY5NjMuOTZzLm1wMyIsImJlYyI6WyJtYWxlIiwibGlmZXN0eWxlIl0sImFmcCI6bnVsbH0%3D",
+    id_author: 1535809342,
+    releaseDate: "2023-07-02T23:30:00Z",
+    trackId: 1000604730376,
+    trackName:
+      "HOW ROLLING LOUD GOT STARTED!: MILLION DOLLAZ WORTH OF GAME EPISODE 227",
+    trackTimeMillis: 5229000,
+    wrapperType: "podcastEpisode",
+  },
+  {
+    description:
+      'We got Harlem\'s very own, Lil TJay in the building. Ever since his days of releasing music on SoundClod, Lil Tjay has captured the hearts of millions with his distinct melodic style and emotionally charged lyrics. With hits like "F.N.," "Leaked," and "Calling My Phone" featuring 6LACK, his music resonates with listeners worldwide, amassing billions of streams and earning him critical acclaim. Since we talking numbers, not many can say they have 18 million monthly listeners after a two year hiatus. This conversation goes beyond the surface, showcasing the depth and authenticity of his artistry. His journey from humble beginnings to becoming a chart-topping artist is a testament to his dedication and raw talent.\n\nYou can find every episode of this show on Apple Podcasts, Spotify or YouTube. Prime Members can listen ad-free on Amazon Music. For more, visit barstool.link/mworthofgame',
+    episodeUrl:
+      "https://dts.podtrac.com/redirect.mp3/landmark-dynamic.barstoolsports.com/stream/4ZoYTtdROX9jDdBccpk81bUb/audio.mp3?v=eyJzcmMiOiJodHRwczovL2JhcnN0b29sLXBvZGNhc3RzLnMzLmFtYXpvbmF3cy5jb20vYmFyc3Rvb2wtc3BvcnRzL21pbGxpb24tZG9sbGF6LXdvcnRoLW9mLWdhbWUvNFpvWVR0ZFJPWDlqRGRCY2NwazgxYlViL0VQLTIyNi1GdC1MaWwtVEpheS0tLWF1ZGlvLXdpdGgtbmFzY2FyZC44NTMwOTg4Ny45NnMubXAzIiwiYmVjIjpbIm1hbGUiLCJsaWZlc3R5bGUiXSwiYWZwIjpudWxsfQ%3D%3D",
+    id_author: 1535809342,
+    releaseDate: "2023-06-25T23:30:00Z",
+    trackId: 1000618282685,
+    trackName: "LIL TJAY",
+    trackTimeMillis: 3429000,
+    wrapperType: "podcastEpisode",
+  },
+];
+export default {
+  podcastListMock,
+  podcastListMockAfterFormated,
+  podcastDetailMock,
+  podcastSelectedMock,
+};
